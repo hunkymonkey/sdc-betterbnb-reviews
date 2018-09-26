2a2b1080baf26f2ea206a53cc5bf227af8a2d81d
Servando J Berna Rico <servandojbernarico@Servandos-MacBook-Pro.local>
2018-09-26 12:25:48 -0700

    populate.js 10M entries 1st try.

--- database/populate.js
+++ database/populate.js
@@ -2,10 +2,12 @@ var { Comment } = require("./models.js");
 
 var jsonData = require("./MOCK_DATA.json");
 
-Comment.collection.insertMany(jsonData, (err, data) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log("Success saving data in database");
-  }
-});
+for (let i = 0; i < 10000; i++) {
+  Comment.collection.insertMany(jsonData, (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Success saving data in database");
+    }
+  });
+}
