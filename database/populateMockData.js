795bcc1d21b579fec20657af6bf6c8769cf1dff9
Servando J Berna Rico <servandojbernarico@Servandos-MacBook-Pro.local>
2018-09-29 10:54:04 -0700

    Mock data creation accelerated. MongoDB data load accelerated.

--- database/populateMockData.js
+++ database/populateMockData.js
@@ -5,27 +5,45 @@ const rating = () => {
   return Math.floor(Math.random() * 10 + 1);
 };
 
-const stream = fs.createWriteStream("./mockdata2.json", { flags: "a" });
+const stream = fs.createWriteStream("./mockdata.json", { flags: "a" });
 
-for (let i = 0; i < 10000000; i++) {
-  if (i % 1000000 === 0) {
-    console.log(i);
-  }
-  const comment = {
-    id: faker.random.number(),
-    User: faker.lorem.word(),
-    Name: faker.name.firstName(),
-    ImageUrl: faker.image.avatar(),
-    Date: faker.date.recent(),
-    AccuracyRating: rating(),
-    CommunicationRating: rating(),
-    CleanlinessRating: rating(),
-    LocationRating: rating(),
-    CheckInRating: rating(),
-    ValueRating: rating(),
-    Text: faker.lorem.paragraph(),
-    House: faker.random.number()
+const populate = writer => {
+  let i = 10000000;
+
+  const write = () => {
+    let ok = true;
+    do {
+      i--;
+      const comment = {
+        id: faker.random.number(),
+        User: faker.lorem.word(),
+        Name: faker.name.firstName(),
+        ImageUrl: faker.image.avatar(),
+        Date: faker.date.recent(),
+        AccuracyRating: rating(),
+        CommunicationRating: rating(),
+        CleanlinessRating: rating(),
+        LocationRating: rating(),
+        CheckInRating: rating(),
+        ValueRating: rating(),
+        Text: faker.lorem.paragraph(),
+        House: faker.random.number()
+      };
+
+      const data = JSON.stringify(comment, null, 4) + "\t";
+      if (i === 0) {
+        writer.write(data);
+      } else {
+        ok = writer.write(data);
+      }
+    } while (i > 0 && ok);
+    {
+      if (i > 0) {
+        writer.once("drain", write);
+      }
+    }
   };
-  const temp = JSON.stringify(comment, null, 4) + "\t";
-  stream.write(temp);
-}
+  write();
+};
+
+populate(stream);
